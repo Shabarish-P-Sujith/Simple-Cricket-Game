cdbf3f2f595d5185075b4cb2cbc1fb0c5e2a2104
Shabarish-P-Sujith <shabarishpsujith@gmail.com>
2024-08-18 00:55:04 +0530

    Added Scores

--- script.js
+++ script.js
@@ -11,32 +11,56 @@ function generateComputerChoice() {
     }    
 }
 
+let score = {
+    win: 0,
+    lose: 0,
+    tie: 0,
+};
+
 function choice(userChoice,computerChoice){
     if (userChoice === 'Bat') {
-        if (computerChoice === userChoice) {
-            resultMsg = '!!! DRAW !!!';
+        if (computerChoice === 'Bat') {
+            resultMsg = '!!! DRAW !!!'; 
+            score.tie++;
         }else if (computerChoice === 'Ball') {
             resultMsg = 'User WON the Game';
+            score.win++;
         }else if (computerChoice === 'Stump') {
             resultMsg = 'Computer WON the Game';
+            score.lose++;
         }
     } 
     else if (userChoice === 'Ball') {
         if(computerChoice === 'Bat'){
             resultMsg = 'Computer WON the Game';
-        }else if(computerChoice === userChoice){
+            score.lose++;
+        }else if(computerChoice === 'Ball'){
             resultMsg = '!!! DRAW !!!';
+            score.tie++;
         }else if(computerChoice === 'Stump'){
             resultMsg = 'User WON the Game';
+            score.win++;
         }
     } 
     else if (userChoice === 'Stump') {
         if(computerChoice === 'Bat'){
             resultMsg = 'User WON the Game';
+            score.win++;
         }else if(computerChoice === 'Ball'){
             resultMsg = 'Computer WON the Game';
-        }else if(computerChoice === userChoice){
+            score.lose++;
+        }else if(computerChoice === 'Stump'){
             resultMsg = '!!! DRAW !!!';
+            score.tie++;
         }
     } 
+}
+
+function finalChoice(userChoice) {
+    console.log(`You have chose ${userChoice}`);
+    generateComputerChoice();
+    console.log(`Computer has chose ${computerChoice}`);
+    choice(userChoice,computerChoice);
+    console.log(resultMsg);
+    console.log(score); console.log('\n');    
 }
